d7b91b421c0a44888ea88909981f0c8af9fae19e
Dusty211 <kylebenz@hotmail.com>
2019-02-08 14:02:59 -0500

    commented car route, basic controller buildouts

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,25 +1,56 @@
 
 class ListingsController < ApplicationController
 
+  before_action :find_params, only: [:edit, :show, :update, :destroy]
+
   def index
+    @listings = Listing.all
   end
 
   def show
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+
+      if @user.save
+        redirect_to @user
+      else
+        render :new
+      end
   end
 
   def edit
   end
 
   def update
+
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @listing.destroy
+    redirect_to listings_path
+  end
+
+  private
+
+  def find_params
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:user_id, :car_id, :description, :title)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 
 class UsersController < ApplicationController
 
+  before_action :find_params, only: [:show, :destroy]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+      if @user.save
+        redirect_to @user
+      else
+        render :new
+      end
   end
 
   # def edit
@@ -20,6 +31,18 @@ class UsersController < ApplicationController
   # end
 
   def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def find_params
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end

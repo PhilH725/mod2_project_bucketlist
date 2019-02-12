f6e4d389a77e1585e0d98908c86be8c57f4e0f65
github username <github email address>
2019-02-12 09:21:40 -0500

    started validations

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,6 +3,15 @@ class Bid < ApplicationRecord
 
   belongs_to :buyer, class_name: "User"
   belongs_to :auction
-  #belongs_to :buyer_auction, class_name: "Auction"
+
+  validates :amount, presence: true
+  validate :get_top_bid
+
+  def get_top_bid
+    @auction = Auction.find(cookies["view_auction"])
+    if bid <= @auction.top_bid
+      errors.add('Bid must be greater than the current top bid')
+    end
+  end
 
 end
